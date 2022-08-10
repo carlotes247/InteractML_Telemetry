142dd4261e9a8dfde6792ae5e8bebc5cdde2f1dc
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2022-08-10 15:38:58 +0100

    Wrote method that loads all telemetry files from a folder

--- Scripts/TelemetryReader.cs
+++ Scripts/TelemetryReader.cs
@@ -5,6 +5,9 @@ using InteractML.Telemetry;
 using ReusableMethods;
 using InteractML;
 using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace InteractML.Telemetry
 {
@@ -14,6 +17,23 @@ namespace InteractML.Telemetry
         
         List<IterationAccuracy> m_AccuraciesPerIteration;
         EasyRapidlib m_EasyRapidlibModel;
+
+        public List<TelemetryData> TelemetryFiles;
+
+        /// <summary>
+        /// Path of folder to search in
+        /// </summary>
+        public string FolderPath;
+
+        // Flags for loading
+        public bool LoadingStarted { get { return m_LoadingStarted; } }
+        [System.NonSerialized]
+        private bool m_LoadingStarted;
+        public bool LoadingFinished { get { return m_LoadingFinished; } }
+        [System.NonSerialized]
+        private bool m_LoadingFinished;
+
+
 
         #endregion
 
@@ -102,6 +122,104 @@ namespace InteractML.Telemetry
 
             return averageAccuracy;
         }
+
+        /// <summary>
+        /// Loads all telemetry files from a path asynchronously
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="specificID"></param>
+        /// <param name="absolutePath"></param>
+        private void LoadAllTelemetryFilesFromPath(string path, string specificID = "", bool absolutePath = false)
+        {
+            if (m_LoadingStarted)
+            {
+                Debug.LogError("Can't start loading when there is a loading in progress...");
+                return;
+            }
+
+
+            if (Directory.Exists(path))
+            {
+                m_LoadingStarted = true;
+                m_LoadingFinished = false;
+                List<TelemetryData> telemetryFiles = new List<TelemetryData>();
+
+                var task = Task.Run(async () =>
+                {
+                    // First, find all the folders
+                    // Iterate to upload all files in folder, including subdirectories
+                    string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                    Debug.Log($"{files.Length + 1} files found. Loading data sets, please wait...");
+                    foreach (string file in files)
+                    {
+                        // If there is a json file, attempt to load
+                        if (Path.GetExtension(file) == ".json")
+                        {
+                            // Are we looking for a specific ID?
+                            if (!string.IsNullOrEmpty(specificID))
+                            {
+                                // skip if the file doesn't contain the ID we want
+                                if (!file.Contains(specificID))
+                                    continue;
+                            }
+
+                            string fileName = Path.GetFileNameWithoutExtension(file);
+                            string filePath = path;
+                            // Load training data set
+                            TelemetryData telemetryFile = new TelemetryData();
+                            telemetryFile = await IMLDataSerialization.LoadObjectFromDiskAsync(telemetryFile, filePath, fileName);
+
+                            // Add to list if not null
+                            if (telemetryFile != null)
+                            {
+                                telemetryFiles.Add(telemetryFile);
+                            }
+                        }
+                    }
+
+                    if (telemetryFiles.Count == 0)
+                    {
+                        Debug.Log("Couldn't load folder!");
+
+                    }
+                    else
+                    {
+                        m_LoadingFinished = true;
+                        m_LoadingStarted = false; // allow to re-load if user wants to
+                        Debug.Log($"{telemetryFiles.Count + 1} Telemetry Files Loaded!");
+                    }
+
+
+                });
+
+                // Starts coroutine that will dump all loaded values into internal list
+                StartCoroutine(LoadTelemetryFilesCoroutine(task));
+
+            }
+            else
+            {
+                Debug.LogError($"The folder {path} doesn't exist!");
+            }
+        }
+
+        /// <summary>
+        /// Awaits until the loading task is finished to dump contents into internal list
+        /// </summary>
+        /// <param name="taskLoading"></param>
+        /// <returns></returns>
+        private IEnumerator LoadTelemetryFilesCoroutine(Task taskLoading)
+        {
+            while (!taskLoading.IsCompleted)
+            {
+                // wait a frame
+                yield return null;
+            }
+
+            Task.FromResult<List<TelemetryData>>(TelemetryFiles);
+        }
+
+
     }
+
 
 }
