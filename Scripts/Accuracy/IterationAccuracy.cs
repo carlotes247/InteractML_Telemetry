a795b57daf9b659c798965e777bb9a01268d3a29
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2022-08-10 14:52:40 +0100

    Timestamps included in accuracy calculations

---
+++ Scripts/Accuracy/IterationAccuracy.cs
@@ -0,0 +1,22 @@
+﻿namespace InteractML.Telemetry
+{
+    /// <summary>
+    /// Holds data linked with the accuracy of interation
+    /// </summary>
+    public struct IterationAccuracy 
+    {
+        /// <summary>
+        /// Usually is the modelID
+        /// </summary>
+        public string IterationID;
+        /// <summary>
+        /// Accuracy of model this iteration
+        /// </summary>
+        public float Accuracy;
+        /// <summary>
+        /// When did this iteration finished?
+        /// </summary>
+        public System.DateTime TimeStamp;
+        
+    }
+}
--- Scripts/TelemetryReader.cs
+++ Scripts/TelemetryReader.cs
@@ -6,79 +6,99 @@ using ReusableMethods;
 using InteractML;
 using System.Linq;
 
-public class TelemetryReader : MonoBehaviour
+namespace InteractML.Telemetry
 {
-    
-    /// <summary>
-    /// Calculate the average accuracy from all iterations in a telemetryFile
-    /// </summary>
-    /// <param name="telemetryFile"></param>
-    /// <returns></returns>
-    private float CalculateAvgAccuracy(TelemetryData telemetryFile)
+    public class TelemetryReader : MonoBehaviour
     {
-        List<float> accuracyIterations = new List<float>();
-        float averageAccuracy = 0f;
-        // If all is good with telemetry file
-        if (telemetryFile != null && !Lists.IsNullOrEmpty(ref telemetryFile.IMLIterations))
-        {
-            // Iterate through IML iterations and 'construct' a complete iteration
-            foreach (var IMLIteration in telemetryFile.IMLIterations)
-            {
-                List<IMLTrainingExample> uniqueClassesList = new List<IMLTrainingExample>();
-                int numUniqueClasses = 0;
-                // if we got both training data and testing data, calculate accuracy for this entry
-                if (!Lists.IsNullOrEmpty(ref IMLIteration.ModelData.TrainingData) && !Lists.IsNullOrEmpty(ref IMLIteration.ModelData.TestingData))
-                {
-                    // create kNN model
-                    EasyRapidlib easyRapidlibModel = new EasyRapidlib(EasyRapidlib.LearningType.Classification);
-                    // add training data to model
-                    easyRapidlibModel.AddTrainingDataSet(IMLIteration.ModelData.TrainingData);
-                    // train kNN model
-                    easyRapidlibModel.TrainModel();
-                    // get list of unique classes in training set
-                    TrainingExamplesNode.SetUniqueClassesFromDataVector(ref uniqueClassesList, ref numUniqueClasses, IMLIteration.ModelData.TrainingData);
-                    // testing entries in list should match uniqueClasses entries
-                    if (uniqueClassesList.Count == IMLIteration.ModelData.TestingData.Count)
-                    {
-                        // go through each testing entry per class. 
-                        foreach (var testingEntry in IMLIteration.ModelData.TestingData)
-                        {
-                            int numHits = 0;
-                            int numMisses = 0;
-                            foreach (var testingExample in testingEntry)
-                            {
-                                var expectedOutput = testingExample.GetOutputs();
-                                double[] output = easyRapidlibModel.Run(testingExample.GetInputs(), testingExample.GetOutputs().Length);
-                                if (Enumerable.SequenceEqual(output, expectedOutput))
-                                {
-                                    // hit
-                                    numHits++;
-                                }
-                                else
-                                {
-                                    // miss
-                                    numMisses++;
-                                }
-                            }
-                            // Calculate accuracy and add to list of accuracies
-                            float accuracyIteration = numHits / (numHits + numMisses);
-                            accuracyIterations.Add(accuracyIteration);
-
-                            Debug.Log($"Accuracy was {accuracyIteration} at {IMLIteration.EndTimeUTC}");
-
-                        }
-
-
-
-                        averageAccuracy = accuracyIterations.Average();                        
-                    }
-                    // We should save this accuracy as well together with the timestamp
-                }                
-            }
-        }
-
-        Debug.Log($"Average accuracy is {averageAccuracy}");
+        #region Variables
         
-        return averageAccuracy;
+        List<IterationAccuracy> m_AccuraciesPerIteration;
+
+        #endregion
+
+        /// <summary>
+        /// Calculate the average accuracy from all iterations in a telemetryFile
+        /// </summary>
+        /// <param name="telemetryFile"></param>
+        /// <returns></returns>
+        private float CalculateAvgAccuracy(TelemetryData telemetryFile, ref List<IterationAccuracy> accuracyIterations)
+        {
+            if (accuracyIterations == null)
+                accuracyIterations = new List<IterationAccuracy>();
+            else
+                accuracyIterations.Clear();
+            List<float> accuracyIterationsFloat = new List<float>();
+            float averageAccuracy = 0f;
+            // If all is good with telemetry file
+            if (telemetryFile != null && !Lists.IsNullOrEmpty(ref telemetryFile.IMLIterations))
+            {
+                // Iterate through IML iterations and 'construct' a complete iteration
+                foreach (var IMLIteration in telemetryFile.IMLIterations)
+                {
+                    List<IMLTrainingExample> uniqueClassesList = new List<IMLTrainingExample>();
+                    int numUniqueClasses = 0;
+                    // if we got both training data and testing data, calculate accuracy for this entry
+                    if (!Lists.IsNullOrEmpty(ref IMLIteration.ModelData.TrainingData) && !Lists.IsNullOrEmpty(ref IMLIteration.ModelData.TestingData))
+                    {
+                        // create kNN model
+                        EasyRapidlib easyRapidlibModel = new EasyRapidlib(EasyRapidlib.LearningType.Classification);
+                        // add training data to model
+                        easyRapidlibModel.AddTrainingDataSet(IMLIteration.ModelData.TrainingData);
+                        // train kNN model
+                        easyRapidlibModel.TrainModel();
+                        // get list of unique classes in training set
+                        TrainingExamplesNode.SetUniqueClassesFromDataVector(ref uniqueClassesList, ref numUniqueClasses, IMLIteration.ModelData.TrainingData);
+                        // testing entries in list should match uniqueClasses entries
+                        if (uniqueClassesList.Count == IMLIteration.ModelData.TestingData.Count)
+                        {
+                            // go through each testing entry per class. 
+                            foreach (var testingEntry in IMLIteration.ModelData.TestingData)
+                            {
+                                int numHits = 0;
+                                int numMisses = 0;
+                                foreach (var testingExample in testingEntry)
+                                {
+                                    var expectedOutput = testingExample.GetOutputs();
+                                    double[] output = easyRapidlibModel.Run(testingExample.GetInputs(), testingExample.GetOutputs().Length);
+                                    if (Enumerable.SequenceEqual(output, expectedOutput))
+                                    {
+                                        // hit
+                                        numHits++;
+                                    }
+                                    else
+                                    {
+                                        // miss
+                                        numMisses++;
+                                    }
+                                }
+                                // Calculate accuracy and add to list of accuracies
+                                // float
+                                float accuracyIteration = numHits / (numHits + numMisses);
+                                accuracyIterationsFloat.Add(accuracyIteration);
+                                // struct with extra info
+                                IterationAccuracy iterationAccuracyStruct;
+                                iterationAccuracyStruct.Accuracy = accuracyIteration;
+                                iterationAccuracyStruct.TimeStamp = IMLIteration.EndTimeUTC;
+                                iterationAccuracyStruct.IterationID = IMLIteration.ModelData.ModelID;
+                                accuracyIterations.Add(iterationAccuracyStruct);
+
+                                Debug.Log($"Accuracy was {accuracyIteration} at {iterationAccuracyStruct.TimeStamp}");
+
+                            }
+
+
+
+                            averageAccuracy = accuracyIterationsFloat.Average();
+                        }
+                        // We should save this accuracy as well together with the timestamp
+                    }
+                }
+            }
+
+            Debug.Log($"Average accuracy is {averageAccuracy}");
+
+            return averageAccuracy;
+        }
     }
+
 }
